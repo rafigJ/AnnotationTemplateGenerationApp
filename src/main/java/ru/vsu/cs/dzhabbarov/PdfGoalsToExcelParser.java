a98789ec144ab95f0603ceb79e2d008c7a5a2da2
Rafig <rafigdzabbarov0410@gmail.com>
2024-06-30 05:58:47 +0300

    add: goals

--- src/main/java/ru/vsu/cs/dzhabbarov/PdfGoalsToExcelParser.java
+++ src/main/java/ru/vsu/cs/dzhabbarov/PdfGoalsToExcelParser.java
@@ -13,11 +13,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @Slf4j
 public class PdfGoalsToExcelParser {
-    private static int i = 0;
 
     public static String extractTextBetweenSections(String text, String startSection, String endSection) {
         int startIndex = text.indexOf(startSection);
